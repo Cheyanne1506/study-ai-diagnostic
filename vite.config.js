b88e6bd4428e56a61531aedeaaf1d22d81464a40
Cheyanne1506 <160475112+Cheyanne1506@users.noreply.github.com>
2026-02-06 20:24:47 +0200

    fix base

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/study-ai-diagnostic/",   // ⭐ IMPORTANT
+  base: "/",   // ⭐ IMPORTANT
   plugins: [react()],
 })
